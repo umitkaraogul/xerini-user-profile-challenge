a93872574ff26c1e202e15eaf2cb9b38f8dab90c
Umit <ukaraogul@gmail.com>
2020-10-14 17:30:01 +0300

    login page design fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AppContext from "../context/AppContext";
 import ProfileService from "../services/ProfileService";
-import { Form, Button, Grid } from "semantic-ui-react";
+import {
+  Form,
+  Button,
+  Grid,
+  Segment,
+  Header,
+  Message,
+} from "semantic-ui-react";
 
 const Login = () => {
   const { appState, setAppState } = useContext(AppContext);
+  const [userName, setUserName] = useState("admin");
+  const [password, setPassword] = useState("password");
+  const [error, setError] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setAppState({
-      ...appState,
-      isLoggedIn: true,
-      users: ProfileService().users,
-    });
+    if (userName === "admin") {
+      setAppState({
+        ...appState,
+        isLoggedIn: true,
+        users: ProfileService().users,
+      });
+    } else {
+      setError("Login account authentication error");
+    }
   };
 
   return (
@@ -22,25 +36,38 @@ const Login = () => {
       verticalAlign="middle"
     >
       <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="violet" textAlign="center">
+          Login to your account
+        </Header>
         <Form size="large" onSubmit={handleSubmit}>
-          <Form.Input
-            fluid
-            icon="user"
-            iconPosition="left"
-            placeholder="Username"
-          />
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+              value={userName}
+              onChange={({ target }) => setUserName(target.value)}
+            />
 
-          <Form.Input
-            fluid
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-          />
-          <Button fluid size="large" type="submit" color="violet">
-            Login
-          </Button>
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <Button fluid size="large" type="submit" color="violet">
+              Login
+            </Button>
+            {error && <Message color="red">{error}</Message>}
+          </Segment>
         </Form>
+        <Message>
+          <a href="https://xerini.co.uk/">Visit</a>
+        </Message>
       </Grid.Column>
     </Grid>
   );
